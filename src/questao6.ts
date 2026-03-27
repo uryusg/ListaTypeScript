aac24d6e976009456b45cc3f3a5ab3d8c1e073a8
Samuel-Santos07 <uryfaculdade@gmail.com>
2026-03-26 22:05:14 -0300

    ultima atualizacao 26/03

---
+++ src/questao6.ts
@@ -0,0 +1,6 @@
+// Crie um programa que receba 5 números e mostre quantos são positivos.
+for(let i:number=0;i<5;i++){
+    let nu1:number=Number(prompt("digite um numero: "))
+    let numero:number= Number(nu1)
+    
+}
